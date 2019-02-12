2bea8d502b53838bf1e64de3dbf46dcf46fc37c6
Joe Johnson <johnson.joseph527@gmail.com>
2019-02-12 15:18:29 -0700

    Added JS for save button and innertext for card
    Co-Authored-By: Bridgett <bacoyle0409@gmail.com>
    Co-authored-by: another-Hannah <another-hannahhyde@gmail.com>

---
+++ idea.js
@@ -0,0 +1,9 @@
+class idea {
+  constructor() {
+    this.title = title.value;
+    this.body = body.value;
+    this.id = id.value;
+    this.quality = quality.value;
+  }
+}
+
--- main.js
+++ main.js
@@ -5,21 +5,41 @@ var searchBtn = document.querySelector('.search-btn');
 
 var title = document.querySelector('#title-box');
 var body = document.querySelector('#body-box');
-var saveBtn = document.querySelector('.save-btn');
+var saveBtn = document.getElementById('save-button');
 
 var downvoteBtn = document.querySelector('#downvote-button');
 var upvoteBtn = document.querySelector('#upvote-button');
-var quality = document.querySelectorAll('.idea-quality');
+var quality = document.querySelectorAll('#idea-quality');
 var ideaCardTemplate = document.getElementById('idea-card-template');
 var ideaCardArea = document.getElementById('idea-card-area');
 
+saveBtn.addEventListener ('click', function(e) {
+  e.preventDefault();
+  addCard();
+  clearFields();
+});
 
 function addCard() {
   var clone = ideaCardTemplate.content.cloneNode(true);
-  ideaCardArea.insertBefore(clone, ideaCardArea.firstChild)
+  clone.getElementById("idea-title").innerText = title.value;
+  clone.getElementById("idea-body").innerText = body.value;
+  ideaCardArea.insertBefore(clone, ideaCardArea.firstChild);
+}
+
+function clearFields() {
+  title.value = "";
+  body.value = "";
 }
 
 // window.onload
 // get cards from local storage
 // parse and append to dom
 
+//counter starts at 0
+//increment counter by plus1 each time upvoteButton is clicked
+//decrease counter by 1 each time downvoteButton is clicked
+//when counter is 0, quality is swill
+//when counter is 1, quality is plaasuible
+//when counter is 2, quality is genius
+//when counter is > 2, counter is 2
+//when counter is < 0, counter is 0
